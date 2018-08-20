e3f06ad83c92d99b221a32ff263e19f443b44148
Lunar100 <42229839+Lunar100@users.noreply.github.com>
2018-08-19 18:41:37 -0700

    Update yeaserver.py

--- yeaserver.py
+++ yeaserver.py
@@ -1,14 +1,10 @@
-#!/usr/bin/env python
-
 import socket
-TCP_IP = '127.0.0.1'
+TCP_IP = '73.70.193.8'
 TCP_PORT = 5005
 BUFFER_SIZE = 20  # Normally 1024, but we want fast response
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
-
 conn, addr = s.accept()
 print ("Connection address:", addr)
 while True:
